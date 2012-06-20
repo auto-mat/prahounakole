d353fec998deac2f1d3c016d01e8cb5f7731ae38
Ales Zoulek <ales.zoulek@gmail.com>
2012-06-20 18:15:35 +0200

    Adding setup.py file for packaging

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+
+VERSION = (0, 1, 0)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+long_description = ''
+try:
+    f = open(path.join(path.dirname(__file__), 'README.md'))
+    long_description = f.read().strip()
+    f.close()
+except:
+    pass
+
+
+setup(
+    name = 'prahounakole',
+    description = "Django aplikace cyklistická mapa Prahou na kole http://mapa.prahounakole.cz/",
+    url = "http://github.com/auto-mat/prahounakole",
+    long_description = long_description,
+    version = __versionstr__,
+    author = "Auto*Mat",
+    author_email = "redakce@prahounakole.cz",
+    license = "BSD",
+    packages = ['cyklomapa', 'pnk'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
+
