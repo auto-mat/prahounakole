0960e9b7f0606c9e07554e800d1ded3cc5a7ca64
Vrata Filler <vrataf@centrum.cz>
2012-10-14 13:03:50 +0200

    /znacky

--- pnk/urls.py
+++ pnk/urls.py
@@ -16,5 +16,7 @@ urlpatterns = patterns('',
     (r'^uzavirky/$', uzavirky_view),
     (r'^uzavirky/feed/$', UzavirkyFeed()),
     (r'^metro/$', metro_view),
+    (r'^znacky/$', znacky_view),
+    (r'^detail/(\d+)/', detail_view),
     (r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
