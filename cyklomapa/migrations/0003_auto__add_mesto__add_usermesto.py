ebc0d675ebbda4c51807bd7ba9039b86a3e0d1da
Petr Dlouhý <petr.dlouhy@email.cz>
2013-02-07 11:06:12 +0100

    nové schemamigration a datamigration pro původní databázi

--- cyklomapa/migrations/0003_auto__add_mesto__add_usermesto.py
+++ cyklomapa/migrations/0003_auto__add_mesto__add_usermesto.py
@@ -8,18 +8,83 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'Poi.mesto'
-        db.add_column('cyklomapa_poi', 'mesto',
-                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['cyklomapa.Mesto']),
-                      keep_default=False)
+        # Adding model 'Mesto'
+        db.create_table('cyklomapa_mesto', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('nazev', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('aktivni', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('vyhledavani', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('zoom', self.gf('django.db.models.fields.PositiveIntegerField')(default=13)),
+            ('uvodni_zprava', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('geom', self.gf('django.contrib.gis.db.models.fields.PointField')()),
+        ))
+        db.send_create_signal('cyklomapa', ['Mesto'])
+
+        # Adding model 'UserMesto'
+        db.create_table('cyklomapa_usermesto', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+        ))
+        db.send_create_signal('cyklomapa', ['UserMesto'])
+
+        # Adding M2M table for field mesta on 'UserMesto'
+        db.create_table('cyklomapa_usermesto_mesta', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('usermesto', models.ForeignKey(orm['cyklomapa.usermesto'], null=False)),
+            ('mesto', models.ForeignKey(orm['cyklomapa.mesto'], null=False))
+        ))
+        db.create_unique('cyklomapa_usermesto_mesta', ['usermesto_id', 'mesto_id'])
 
 
     def backwards(self, orm):
-        # Deleting field 'Poi.mesto'
-        db.delete_column('cyklomapa_poi', 'mesto_id')
+        # Deleting model 'Mesto'
+        db.delete_table('cyklomapa_mesto')
+
+        # Deleting model 'UserMesto'
+        db.delete_table('cyklomapa_usermesto')
+
+        # Removing M2M table for field mesta on 'UserMesto'
+        db.delete_table('cyklomapa_usermesto_mesta')
 
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         'cyklomapa.legenda': {
             'Meta': {'object_name': 'Legenda'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -30,6 +95,7 @@ class Migration(SchemaMigration):
         },
         'cyklomapa.mesto': {
             'Meta': {'object_name': 'Mesto'},
+            'aktivni': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'geom': ('django.contrib.gis.db.models.fields.PointField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'nazev': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
@@ -46,7 +112,6 @@ class Migration(SchemaMigration):
             'foto_thumb': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'geom': ('django.contrib.gis.db.models.fields.PointField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mesto': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['cyklomapa.Mesto']"}),
             'nazev': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'remark': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cyklomapa.Status']"}),
@@ -70,6 +135,12 @@ class Migration(SchemaMigration):
             'misto': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cyklomapa.Poi']", 'null': 'True', 'blank': 'True'}),
             'status': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
             'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'cyklomapa.usermesto': {
+            'Meta': {'object_name': 'UserMesto'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mesta': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['cyklomapa.Mesto']", 'symmetrical': 'False'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
         },
         'cyklomapa.vrstva': {
             'Meta': {'ordering': "['order']", 'object_name': 'Vrstva'},
