531a726c780679bd65dd8aea52242aa84fcc1d8f
Petr Dlouhý <petr.dlouhy@email.cz>
2013-06-01 15:52:46 +0200

    fix wsgi after reorganization

--- wsgi.py
+++ wsgi.py
@@ -14,8 +14,27 @@ framework.
 
 """
 import os
+import site
+import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pnk.settings")
+ALLDIRS = [ 'env/lib/python2.6/site-packages', ]
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path)
+
+# Add each new site-packages directory.
+for directory in ALLDIRS:
+  site.addsitedir(directory)
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pnk.settings_local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
