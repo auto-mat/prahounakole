f5fc268237ed77b12b5351a9dcaa4e27db103220
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-11 16:40:49 +0200

    update update.sh script

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-sudo git pull origin master
-env/bin/pip install -r requirements.txt
-sudo env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d"`-mapa.json
-sudo env/bin/python manage.py migrate
-sudo env/bin/python manage.py collectstatic --noinput
+git pull
+env/bin/pip install -r requirements
+if [ "$1" = "migrate" ]; then
+   echo "Backuping db..."
+   mkdir db_backup
+   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-aklub.json
+   echo "Migrating..."
+   env/bin/python manage.py migrate
+fi
+(cd apps/aklub/ && django-admin.py compilemessages)
+env/bin/python manage.py collectstatic --noinput
 touch wsgi.py
-sudo /etc/init.d/memcached restart
