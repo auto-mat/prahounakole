de34b9460cd03349ef3790fc4cb9d708866cea3b
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-14 13:21:08 +0200

    fix update script + .gitignore

--- update.sh
+++ update.sh
@@ -5,10 +5,10 @@ env/bin/pip install -r requirements
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir db_backup
-   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-aklub.json
+   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-cyklomapa.json
    echo "Migrating..."
    env/bin/python manage.py migrate
 fi
-(cd apps/aklub/ && django-admin.py compilemessages)
+(cd apps/cyklomapa/ && django-admin.py compilemessages)
 env/bin/python manage.py collectstatic --noinput
 touch wsgi.py
