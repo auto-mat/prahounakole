31de37c6a0616a57759243c5ee1734294bc09f97
Petr Dlouhý <petr.dlouhy@email.cz>
2014-03-23 07:22:36 +0100

    fix cache_page_mesto decorator

--- apps/cyklomapa/views.py
+++ apps/cyklomapa/views.py
@@ -94,7 +94,7 @@ def cache_page_mesto(expiration):
        def wrapper(*args, **kwargs):
           cache = get_cache('default')
 
-          cache_key = 'kml_view_' + args[1] + '_' + args[0].mesto.slug
+          cache_key = 'kml_view_' + args[1] + '_' + args[0].mesto.sektor.slug
           result = cache.get(cache_key)
           if result == None:
              result = fn(*args, **kwargs)
