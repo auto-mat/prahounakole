3baf2fa4ed70f85dbb21f0355276c7113a87561d
Petr Dlouhý <petr.dlouhy@email.cz>
2014-10-16 11:34:30 +0200

    improve update script

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#version 0.2
+#version 0.3
+
+app_name=pnk
+db_name=pnk
+source update_local.sh
 
 set -e
 
@@ -10,13 +14,16 @@ fi
 
 git pull
 source env/bin/activate
-env/bin/python env/bin/pip install --process-dependency-links -r requirements.txt --upgrade
+env/bin/python env/bin/pip install --process-dependency-links -r requirements.txt
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir -p db_backup
-   sudo -u postgres pg_dump pnk > db_backup/`date +"%y%m%d-%H:%M:%S"`-pnk.sql
+   sudo -u postgres pg_dump $db_name > db_backup/`date +"%y%m%d-%H:%M:%S"`-pnk.sql
    echo "Migrating..."
    env/bin/python ./manage.py migrate
 fi
 env/bin/python ./manage.py collectstatic --noinput
 touch wsgi.py
+sudo supervisorctl restart $app_name
+
+echo "App succesfully updated"
