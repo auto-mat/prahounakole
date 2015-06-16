bafa7a507e7fd862b42db378896b7fda6d6d4fe8
Petr Dlouhý <petr.dlouhy@email.cz>
2015-06-16 16:17:56 +0200

    add SECRET_KEY to settings for Travis

--- project/settings.py
+++ project/settings.py
@@ -194,6 +194,8 @@ REST_ENABLED=True
 FLUENT_COMMENTS_EXCLUDE_FIELDS = ('url',)
 COMMENTS_APP = 'fluent_comments'
 
+SECRET_KEY = 'change_me'
+
 # import local settings
 try:
     from settings_local import *
