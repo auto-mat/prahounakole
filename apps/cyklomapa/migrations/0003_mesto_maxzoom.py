a25a0347e6f433ec165ecb8be9218c52dd3157fe
Petr Dlouhý <petr.dlouhy@email.cz>
2015-07-22 21:39:57 +0200

    add migration for previous commit

---
+++ apps/cyklomapa/migrations/0003_mesto_maxzoom.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cyklomapa', '0002_auto_20150616_1610'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='mesto',
+            name='maxzoom',
+            field=models.PositiveIntegerField(default=18, help_text='Maxim\xe1ln\xed zoomlevel mapy'),
+        ),
+    ]
