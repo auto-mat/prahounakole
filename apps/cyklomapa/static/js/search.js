bbbe1a6d85f65b44c0db8c5b20b2ded4a66e4c32
Petr Dlouhý <petr.dlouhy@email.cz>
2018-03-26 11:45:25 +0200

    emergency fix for autocomplete with Mapbox

--- apps/cyklomapa/static/js/search.js
+++ apps/cyklomapa/static/js/search.js
@@ -1,24 +1,24 @@
-var geoCodeURL = "https://search.mapzen.com/v1/autocomplete";
+var geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 search_options = {
        source: function ( request, response ) {
            $.ajax({
-                  url: geoCodeURL,
+                  url: geoCodeURL + request.term + ".json",
                   data: {
                       format: "json",
                       // Focus pointy nefungují dobře, protože se tam pak nenabízejí ulice a města, ale spousta adresních bodů.
-                      //"focus.point.lat": map.getCenter().transform(EPSG900913, EPSG4326).lat,
-                      //"focus.point.lon": map.getCenter().transform(EPSG900913, EPSG4326).lon,
-                      api_key: "mapzen-dsCsXWh",
-                      "boundary.country": "CZE",
+                      "proximity": map.getCenter().transform(EPSG900913, EPSG4326).lat + "," + map.getCenter().transform(EPSG900913, EPSG4326).lon,
+                      access_token: "pk.eyJ1IjoicGV0cmRsb3VoeSIsImEiOiJjajhvbHE0OGQwNHFnMnhxY252NDYzaHRjIn0.Xn34k0Fd0zzjvfJku892aw",
+                      "country": "cz",
+                      "language": "CS_cz",
                       text: request.term
                   },
                   success: function ( data ) {
                       item_map = $.map( data.features, function( item ) {
                           return {
-                              label: item.properties.label,
-                              value: item.properties.label,
-                              lat: String(item.geometry.coordinates[1]),
-                              lon: String(item.geometry.coordinates[0])
+                              label: item.place_name,
+                              value: item.place_name,
+                              lat: String(item.center[1]),
+                              lon: String(item.center[0])
                           }}
                       )
                       response(item_map);
