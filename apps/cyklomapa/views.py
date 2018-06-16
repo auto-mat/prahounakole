167bccd874faddb204b32e8b7c91b1bf944b63a5
Timothy Hobbs <timothy@hobbs.cz>
2018-06-16 23:01:52 +0200

    is_authenticated is not a function

--- apps/cyklomapa/views.py
+++ apps/cyklomapa/views.py
@@ -37,7 +37,7 @@ def mapa_view(request, poi_id=None):
         'presets': MapPreset.objects.filter(status__show=True),
         'mesta': Mesto.objects.filter(aktivni=True).order_by('sektor__name').all(),
     }
-    if not (request.mesto and request.mesto.aktivni) and not request.user.is_authenticated():
+    if not (request.mesto and request.mesto.aktivni) and not request.user.is_authenticated:
         return render(request, 'neaktivni.html', context=context)
 
     return render(request, 'mapa.html', context=context)
