d1cceb7d9b8e3659deaa78bb5915947cf50fe0c3
Petr Dlouhý <petr.dlouhy@email.cz>
2018-11-06 16:34:58 +0100

    remove virtualenv from manage.py

--- manage.py
+++ manage.py
@@ -2,18 +2,6 @@
 import os
 import sys
 
-# BEGIN activacte virtualenv
-from project.settings import PROJECT_DIR, normpath
-
-try:
-    activate_path = normpath(PROJECT_DIR, 'env/bin/activate_this.py')
-    with open(activate_path) as f:
-        code = compile(f.read(), "somefile.py", 'exec')
-        exec(code, dict(__file__=activate_path))  # noqa
-except IOError:
-    print("E: virtualenv must be installed to PROJECT_DIR/env")
-# END activacte virtualenv
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
