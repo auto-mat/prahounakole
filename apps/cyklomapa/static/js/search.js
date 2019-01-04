344dd359d73c46c868f5f3b38d79aac8c4887d44
Timothy Hobbs <timothy@hobbs.cz>
2019-01-04 17:47:39 +0100

    Switch to using here.com geocoder

--- apps/cyklomapa/static/js/search.js
+++ apps/cyklomapa/static/js/search.js
@@ -1,24 +1,27 @@
-var geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+var suggestionsURL = "https://autocomplete.geocoder.api.here.com/6.2/suggest.json";
+var geocodeURL = "https://geocoder.api.here.com/6.2/geocode.json";
+var app_id = "HPfz15EtL6weSct5rbvJ";
+var app_code = "Bm2A0SrXWE0xZ3nhtTQVfg";
 search_options = {
        source: function ( request, response ) {
            $.ajax({
-                  url: geoCodeURL + request.term + ".json",
+                  url: suggestionsURL,
                   data: {
                       format: "json",
                       // Focus pointy nefungují dobře, protože se tam pak nenabízejí ulice a města, ale spousta adresních bodů.
-                      "proximity": map.getCenter().transform(EPSG900913, EPSG4326).lat + "," + map.getCenter().transform(EPSG900913, EPSG4326).lon,
-                      access_token: "pk.eyJ1IjoicGV0cmRsb3VoeSIsImEiOiJjajhvbHE0OGQwNHFnMnhxY252NDYzaHRjIn0.Xn34k0Fd0zzjvfJku892aw",
-                      "country": "cz",
-                      "language": "CS_cz",
-                      text: request.term
+                      "prox": map.getCenter().transform(EPSG900913, EPSG4326).lat + "," + map.getCenter().transform(EPSG900913, EPSG4326).lon,
+                      app_id: app_id,
+                      app_code: app_code,
+                      "country": "CZE",
+                      "language": "cs",
+                      query: request.term
                   },
                   success: function ( data ) {
-                      item_map = $.map( data.features, function( item ) {
+                      item_map = $.map( data.suggestions, function( item ) {
                           return {
-                              label: item.place_name,
-                              value: item.place_name,
-                              lat: String(item.center[1]),
-                              lon: String(item.center[0])
+                              label: item.label,
+                              value: item.label,
+                              locationid: item.locationId
                           };
                       });
                       response(item_map);
@@ -30,32 +33,45 @@ search_options = {
         select: function (e, ui) {
             ga('send', 'event', 'search', 'select', e.target.id);
             $(e.target).val(ui.item.label);
-            lonlat = new OpenLayers.LonLat( ui.item.lon, ui.item.lat).transform(
-                  new OpenLayers.Projection("EPSG:4326"),
-                  map.getProjectionObject()
-            );
-            map_move_by_code = true;
-            if (e.target.id == "mapStreetSearch")
-               zoom_to_level = 16; // Můžeme zvýšit na 18, pokud bude vše vyrenderováno
-            else
-               zoom_to_level = 16;
-            map.setCenter(lonlat, zoom_to_level);
-            map_move_by_code = false;
-            if (appMode != 'routing') {
-                return false;
-            }
-            if (e.target.id == "jpStartStreetSearch") {
-                marker = startMarker;
-            } else {
-                marker = endMarker;
-            }
-            curpos = marker.geometry;
-            marker.geometry.move(lonlat.lon - curpos.x, lonlat.lat - curpos.y);
-            setWaypoint(marker);
-            if (e.target.id == "jpFinishStreetSearch" && waypoints[0] != undefined) {
-                planJourney();
-            }
-            return false;
+
+            $.ajax({
+                  url: geocodeURL,
+                  data: {
+                      format: "json",
+                      // Focus pointy nefungují dobře, protože se tam pak nenabízejí ulice a města, ale spousta adresních bodů.
+                      app_id: app_id,
+                      app_code: app_code,
+                      locationid: ui.item.locationid
+                  },
+                  success: function ( data ) {
+                      lonlatdict = data.Response.View[0].Result[0].Location.DisplayPosition
+                      lonlat = new OpenLayers.LonLat(lonlatdict.Longitude , lonlatdict.Latitude).transform(
+                            new OpenLayers.Projection("EPSG:4326"),
+                            map.getProjectionObject()
+                      );
+                      map_move_by_code = true;
+                      if (e.target.id == "mapStreetSearch")
+                         zoom_to_level = 16; // Můžeme zvýšit na 18, pokud bude vše vyrenderováno
+                      else
+                         zoom_to_level = 16;
+                      map.setCenter(lonlat, zoom_to_level);
+                      map_move_by_code = false;
+                      if (appMode != 'routing') {
+                          return false;
+                      }
+                      if (e.target.id == "jpStartStreetSearch") {
+                          marker = startMarker;
+                      } else {
+                          marker = endMarker;
+                      }
+                      curpos = marker.geometry;
+                      marker.geometry.move(lonlat.lon - curpos.x, lonlat.lat - curpos.y);
+                      setWaypoint(marker);
+                      if (e.target.id == "jpFinishStreetSearch" && waypoints[0] != undefined) {
+                          planJourney();
+                      }
+                  }
+            });
         },
         open: function (e) {
             ga('send', 'event', 'search', 'open', e.target.id);
@@ -69,7 +85,7 @@ search_options = {
             ga('send', 'event', 'search', 'focus', e.target.id);
         },
         //selectFirst: true,
-        autoFocus: true 
+        autoFocus: true
 };
 
 /*
