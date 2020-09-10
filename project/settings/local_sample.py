bd8a8f4a9baa46564f83d8dd191432a2fbd9f45b
Timothy Hobbs <timothy@hobbs.cz>
2020-09-10 19:12:12 +0200

    Cut down local_sample to make debugging travis simpler

--- project/settings/local_sample.py
+++ project/settings/local_sample.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import raven
 from settings import *  # noqa
-from settings import LOGGING, PROJECT_DIR, TEMPLATES, normpath
 
 # Tento soubor překopírujte s názvem settings_local.py a doplňte vaše lokální nastavení
 
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
-SERVER_EMAIL = 'pnk-technik@auto-mat.cz'
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
@@ -32,34 +29,14 @@ GOOGLE_MAPS_API_KEY = 'abcdefg'
 
 SECRET_KEY = 'change_me'
 
-# Don't forget to use absolute paths, not relative paths.
-# TEMPLATE_DIRS.append(os.path.join(PROJECT_DIR, 'env/lib/python2.6/site-packages/debug_toolbar/templates'))
-
-# INSTALLED_APPS.append('debug_toolbar')
-
-
-# def custom_show_toolbar(request):
-#     return True  # Always show toolbar, for example purposes only.
-#
-# DEBUG_TOOLBAR_CONFIG = {
-#     'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-#     'HIDE_DJANGO_SQL': False,
-# }
 
 THUMBNAIL_DEBUG = True
-
-ENABLE_API_PROXY = True
 
 # Make log in execution directory when testing
 LOGGING['handlers']['logfile']['filename'] = normpath(PROJECT_DIR, "pnk.log")
 
-MEDIA_ROOT = 'media/'
-STATIC_ROOT = 'static/'
-
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/pnk-emails'
-
-# FORCE_SUBDOMAIN = "ostrava"
 
 ALLOWED_HOSTS = [
     'mapa.prahounakole.cz',
@@ -67,14 +44,6 @@ ALLOWED_HOSTS = [
     "mapa.localhost",
     "testing-sector.testserver",
 ]
-
-# COMPRESS_ROOT = normpath(PROJECT_DIR, "apps/cyklomapa/static")
-# COMPRESS_ENABLED = False
-
-RAVEN_CONFIG = {
-    'dsn': '',
-    'release': raven.fetch_git_sha(PROJECT_DIR),
-}
 
 CSRF_COOKIE_SECURE = False
 SECURE_BROWSER_XSS_FILTER = False
