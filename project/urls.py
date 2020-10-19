45d4a32dc690370b3a5470e82d305958be189e3f
Timothy Hobbs <timothy@hobbs.cz>
2020-10-19 17:09:24 +0200

    Not sure how to fix travis and have the images still work

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
     url(r'^captcha/', include('captcha.urls')),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
