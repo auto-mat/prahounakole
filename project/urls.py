fd72244b45a37a048f23a1dd0a39e29972582dd7
Timothy Hobbs <timothy@hobbs.cz>
2020-10-19 16:46:04 +0200

    Try to fix travis error

--- project/urls.py
+++ project/urls.py
@@ -6,9 +6,8 @@ from django.contrib.auth import views as auth_views
 
 admin.autodiscover()
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += [
+urlpatterns = [
     url(r'^admin/passreset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^admin/passresetdone/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^admin/passresetconfirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
@@ -20,3 +19,7 @@ urlpatterns += [
     url(r'^', include("cyklomapa.urls")),
     url(r'^captcha/', include('captcha.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
