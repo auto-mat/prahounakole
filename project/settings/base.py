32e708564bcdbbd59fc501de55b779eafb8e4b85
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-02-18 12:48:53 +0100

    project/settings: enable reading / writing MEDIA files to AWS S3 Bucket storage
    (for correct loading of 'Poi' model photos to webmap / panel)

--- project/settings/base.py
+++ project/settings/base.py
@@ -370,16 +370,20 @@ FEEDBACK_CAPTCHAS = [
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'cyklomapa-media')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'cyklomapa-test-media')
 AWS_QUERYSTRING_AUTH = os.environ.get('AWS_QUERYSTRING_AUTH', True)
 AWS_QUERYSTRING_EXPIRE = os.environ.get('AWS_QUERYSTRING_EXPIRE', 60 * 60 * 24 * 365 * 10)
-AWS_DEFAULT_ACL = "private"
+AWS_DEFAULT_ACL = None
 
 
 if AWS_ACCESS_KEY_ID:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_SES_REGION_NAME = 'eu-west-1'
+    AWS_SES_REGION_NAME = os.environ.get('AWS_SES_REGION_NAME', 'eu-west-1')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-{AWS_SES_REGION_NAME}.amazonaws.com'
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+    DEFAULT_FILE_STORAGE = 'cyklomapa.storage_backends.AwsS3MediaStorage'
+    THUMBNAIL_DEFAULT_STORAGE = 'cyklomapa.storage_backends.AwsS3MediaStorage'
+    THUMBNAIL_SUBDIR = 'thumbs'
+
     AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
     EMAIL_BACKEND = 'django_ses.SESBackend'
     MEDIA_ROOT = ""
