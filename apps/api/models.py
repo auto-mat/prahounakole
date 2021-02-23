934e99028005b248620d420ba2bf30d501f26059
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-02-23 09:25:59 +0100

    api/models: add 'Scopes', 'ScopesDefinition' model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import Group
 from django.db import models
 
 # Create your models here.
+
+
+class ScopesDefinition(models.Model):
+    scope = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.scope
+
+
+class Scopes(models.Model):
+    group = models.ManyToManyField(Group)
+    scopes = models.ManyToManyField(ScopesDefinition)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return ' '.join(
+            list(
+                self.scopes.all().values_list('scope', flat=True),
+            ),
+        )
