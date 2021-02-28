a7e60e70b3773b489f9158738582f1090aec459d
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-02-28 20:55:49 +0100

    load_geodata.sh: fix cmd param arg syntax

--- load_geodata.sh
+++ load_geodata.sh
@@ -12,7 +12,7 @@ CZ_REGIONS_EPSG_5514="cz_regions.db"
 git clone https://github.com/ctu-geoforall-lab/gdal-vfr.git
 cd gdal-vfr/
 # Download Vusc (Czechia regions) layer
-./vfr2ogr --type ST_UKSH --format "SQLite" --layer Vusc --geom OriginalniHranice --dsn "$('pwd')/data/${CZ_REGIONS_EPSG_5514}" --overwrite
+./vfr2ogr --type ST_UKSH --format SQLite --layer Vusc --geom OriginalniHranice --dsn "$('pwd')/data/${CZ_REGIONS_EPSG_5514}" --overwrite
 cd data/
 # Coordinate system conversion EPSG 5514 (S-JTSK) -> EPSG 5326
 ogr2ogr -t_srs EPSG:4326 -f SQLite "${CZ_REGIONS_EPSG_4326}" "${CZ_REGIONS_EPSG_5514}"
