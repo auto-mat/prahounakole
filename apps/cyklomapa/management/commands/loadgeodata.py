cfc5ea87ab2c7775b921b3c0f7540660be1bedec
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-03-11 10:22:04 +0100

    cyklomapa/loadgeodata: refactor import 'Czechia regions' (Kraje) geodata layer from RUIAN DB

--- apps/cyklomapa/management/commands/loadgeodata.py
+++ apps/cyklomapa/management/commands/loadgeodata.py
@@ -1,10 +1,15 @@
 import pathlib
 import subprocess
+import tempfile
 
 from django.conf import settings
+from django.contrib.gis.geos import GeometryCollection, LineString
 from django.contrib.gis.utils import LayerMapping
 from django.core.management import BaseCommand, CommandError
 from django.db import connection
+from django.utils.translation import gettext as _
+
+from webmap.models import  Marker, OverlayLayer, Poi
 
 from cyklomapa.models import CzechiaRegions, czechiaregions_mapping
 
@@ -13,30 +18,141 @@ class Command(BaseCommand):
     """Load/update geodata"""
     help = 'Load/update geodata' # noqa
 
-    def handle(self, *args, **options):
-        filename = "cz_regions_EPSG_4326.db"
-        update_geodata = pathlib.Path(settings.BASE_DIR) / "load_geodata.sh"
+    def _import_geodata(self, model, model_mapping, geodata_path):
+        """Import geodata into model
+
+        :param model obj: model object
+        :param model_mapping dict: model fields mapping
+        :param geodata_path str: geodata layer path
+        """
+        lm = LayerMapping(model=model, data=str(geodata_path),
+                          mapping=model_mapping, transform=False)
+        lm.save(strict=True, verbose=False)
+
+    def _import_geodata_grom_ruian_db(
+            self, layer, output_filename, temp_dir,
+    ):
+        """Import geodata layer from RUIAN DB
+
+        :param layer str: name of RUIAN DB geodata layer
+        :param output_filename str: output geodata filename
+        :param temp_dir str: temp dir path
+
+        :return geodata str: geodata layer path (exported from RUIAN DB)
+        """
+
+        geodata = (pathlib.Path(temp_dir) / "gdal-vfr" /
+                   "data" / output_filename)
+
+        import_geodata = (pathlib.Path(__file__).parent.absolute() /
+                          "import_geodata_from_ruian_db.sh")
+        p = subprocess.Popen(
+            [import_geodata, layer, output_filename, temp_dir],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+
+        if p.returncode != 0:
+            if stderr:
+                raise CommandError(
+                    _("Error executing command '%(script)s'\n"
+                      "'%(error)s'") % {
+                          "script": import_geodata,
+                          "error": stderr.decode('utf-8'),
+                      }
+                )
+
+        return geodata
+
+    def _import_cz_regions(self, temp_dir):
+        """Import Czechia regions (Kraje) geodata layer from RUIAN DB
+
+        :param temp_dir str: temp dir path
+        """
+        filename = "cz_regions.db"
+        layer = "Vusc"  # kraje
 
         CzechiaRegions.objects.all().delete()
         with connection.cursor() as cursor:
             q = f"TRUNCATE {CzechiaRegions._meta.db_table} RESTART IDENTITY"
             cursor.execute(q)
 
-        czechia_regions = (pathlib.Path(settings.BASE_DIR) / "gdal-vfr" /
-                           "data" / filename)
+        regions_geodata_path = self._import_geodata_grom_ruian_db(
+            layer=layer, output_filename=filename, temp_dir=temp_dir,
+        )
 
-        p = subprocess.Popen([update_geodata, filename],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate()
+        self._import_geodata(
+            model=CzechiaRegions, model_mapping=czechiaregions_mapping,
+            geodata_path=regions_geodata_path,
+        )
+        self.stdout.write(_("'%(model)s' model(s) created.") % {
+            "model": CzechiaRegions.__name__}
+        )
 
-        if p.returncode != 0:
-            if stderr:
-                raise CommandError(
-                    f"Error executing command '{update_geodata}'\n"
-                    f"{stderr.decode('utf-8')}",
+
+        # Create Poi regions (line geom type representation)
+        overlay, created = OverlayLayer.objects.get_or_create(
+            name="Kraj", desc="CZ Kraje",
+        )
+        marker, created = Marker.objects.get_or_create(
+            name="Kraj", desc="CZ Kraje", line_width=5.0,
+            line_color="#ff0000", layer=overlay
+        )
+
+        objs = []
+        count = 0
+        for r in CzechiaRegions.objects.all():
+            p, created = Poi.objects.get_or_create(
+                name=r.nazev, marker=marker,
+                geom=GeometryCollection(
+                    LineString(r.geom.boundary.coords[0]),
+                ),
+            )
+            count += 1
+            if not created:
+                p.geom = GeometryCollection(
+                    LineString(r.geom.boundary.coords[0]),
                 )
+                objs.append(p)
 
-        lm = LayerMapping(CzechiaRegions, str(czechia_regions),
-                          czechiaregions_mapping, transform=False)
-        lm.save(strict=True, verbose=True)
+        message = _("%(feat)s of 'Poi' models (represented Czechia regions - "
+                    " Kraje) with line geometry type") % {
+                        "feat": len(objs) if objs else count}
+
+        if objs:
+            CzechiaRegions.objects.bulk_update(objs, ["geom"])
+            self.stdout.write(_("%(message)s was %(action)s.") % {
+                "message": message,
+                "action": "updated"}
+            )
+        else:
+            self.stdout.write(_("%(message)s was %(action)s.") % {
+                "message": message,
+                "action": "created"}
+            )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--geodata",
+            dest="geodata",
+            choices=("cz_regions",),
+            help=_("Choose geodata for import"),
+        )
+
+    def handle(self, *args, **options):
+        cz_regions = options.get("geodata")
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # CZ Regions (Kraje)
+            if cz_regions:
+                self.stdout.write(
+                    _("Try import '%(layer)s' geodata layer.") %
+                    {"layer": cz_regions}
+                )
+                self._import_cz_regions(temp_dir=temp_dir)
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        _("Import of geodata layer '%(layer)s' was"
+                          " successful.") % {"layer": cz_regions}
+                    ),
+                )
--- load_geodata.sh
+++ load_geodata.sh
@@ -6,13 +6,18 @@ http://freegis.fsv.cvut.cz/gwiki/RUIAN_/_GDAL#VFR_konverzn.C3.AD_skripty
 http://freegis.fsv.cvut.cz/gwiki/RUIAN#P.C5.99ehled_poskytovan.C3.BDch_VFR_soubor.C5.AF
 '
 
-CZ_REGIONS_EPSG_4326=$1
-CZ_REGIONS_EPSG_5514="cz_regions.db"
+LAYER=$1
+OUTPUT_GEODATA_EPSG_4326=$2
+IFS="."
+read -a STARR <<< "$OUTPUT_GEODATA_EPSG_4326"
+OUTPUT_GEODATA_EPSG_5514="${STARR[0]}_EPSG_5514.db"
+TEMP_DIR=$3
 
+cd $TEMP_DIR
 git clone https://github.com/ctu-geoforall-lab/gdal-vfr.git
 cd gdal-vfr/
 # Download Vusc (Czechia regions) layer
-./vfr2ogr --type ST_UKSH --format SQLite --layer Vusc --geom OriginalniHranice --dsn "$('pwd')/data/${CZ_REGIONS_EPSG_5514}" --overwrite
+./vfr2ogr --type ST_UKSH --format SQLite --layer $LAYER --geom OriginalniHranice --dsn "$('pwd')/data/${OUTPUT_GEODATA_EPSG_5514}" --overwrite
 cd data/
-# Coordinate system conversion EPSG 5514 (S-JTSK) -> EPSG 5326
-ogr2ogr -t_srs EPSG:4326 -f SQLite "${CZ_REGIONS_EPSG_4326}" "${CZ_REGIONS_EPSG_5514}"
+# Coordinate system conversion EPSG 5514 (S-JTSK) -> EPSG 4326 (WGS-84)
+ogr2ogr -t_srs EPSG:4326 -f SQLite "${OUTPUT_GEODATA_EPSG_4326}" "${OUTPUT_GEODATA_EPSG_5514}"
