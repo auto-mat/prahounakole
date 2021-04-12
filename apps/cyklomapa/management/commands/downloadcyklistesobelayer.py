2ca03042857c30f41f33a43859810c5a075e5095
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-12 13:50:40 +0200

    cyklomapa/management/commands/downloadcyklistesobelayer.py: simplify code

--- apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
+++ apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
@@ -5,13 +5,13 @@ from django.conf import settings
 from django.core.management import BaseCommand
 
 from cyklomapa.utils import parse_cykliste_sobe_features
-
+from cyklomapa.views import get_cyklisty_sobe_layer
 
 class Command(BaseCommand):
     """Download cykliste sobe features JSON layer files"""
     help = "Download cykliste sobe features JSON layer files" # noqa
 
     def handle(self, *args, **options):
-        features = parse_cykliste_sobe_features()
-        with open(pathlib.Path(settings.STATIC_ROOT) / "list.json", "w") as f:
-            json.dump(features, f)
+        parse_cykliste_sobe_features(
+            save_to_file=get_cyklisty_sobe_layer.features_file_path,
+        )
