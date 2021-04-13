b37fea5ce87fe66818f4032f9a58729c640e3098
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-13 13:04:58 +0200

    docker-entrypoint.sh: create and load crontab with Django DB global variables
    Required for correct run cronjobs, which has access to the DB
    ('downloadcyklistesobelayer' cronjob).

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-pipenv run python3 manage.py migrate
+# Add variables into crontab
+CRONTAB_FILE=/tmp/crontab
+if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
+su test -c 'for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;'
+su test -c "crontab -l >> "$CRONTAB_FILE""
+su test -c "crontab "$CRONTAB_FILE""
 exec /usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
