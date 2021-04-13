a350d4aea620092d2da787b8636e1fc123e95aa6
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-13 13:20:15 +0200

    docker-entrypoint.sh: simplify create and load crontab file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,10 @@
 # Add variables into crontab
 CRONTAB_FILE=/tmp/crontab
 if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
-su test -c 'for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;'
-su test -c "crontab -l >> "$CRONTAB_FILE""
-su test -c "crontab "$CRONTAB_FILE""
+su test -c '
+for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;
+crontab -l >> '$CRONTAB_FILE'
+crontab '$CRONTAB_FILE''
 # Django migrate
 su test -c "cd /home/test; pipenv run python3 manage.py migrate"
 # Run supervisord
