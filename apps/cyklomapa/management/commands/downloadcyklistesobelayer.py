8943ec262be8ae0c991ea5fe383c267fb951619e
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-13 12:53:41 +0200

    cyklomapa/management/commands/downloadcyklistesobelayer.py: add '--task' command argument
    Allow download cykliste sobe features layer JSON ('--task download_only')
    during build Docker image, when DB is not available.

--- apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
+++ apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
@@ -3,6 +3,7 @@ import pathlib
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.utils.translation import gettext as _
 
 from django_q.tasks import async_task
 
@@ -16,21 +17,40 @@ class Command(BaseCommand):
     """Download cykliste sobe features JSON layer files"""
     help = "Download cykliste sobe features JSON layer files" # noqa
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--task",
+            dest="task",
+            choices=("download_only",),
+            help=_("Choose task"),
+        )
+
     def handle(self, *args, **options):
-        def download():
-            async_task(func=get_cyklisty_sobe_layer.get_cs_features_layer_func,
-                       cache_key=get_cyklisty_sobe_layer.cache_key,
-                       cache_time=get_cyklisty_sobe_layer.long_cache_time,
-                       save_to_file=get_cyklisty_sobe_layer.features_file_path,
-                       save=True)
-
-        all_job_db_result, job_db_result, time_delta = \
-            check_download_cykliste_sobe_layer_job()
-
-        if job_db_result:
-            if (time_delta.total_seconds() >= get_cyklisty_sobe_layer.short_cache_time):
-                download()
-            elif not get_cyklisty_sobe_layer.features_file_path.is_file():
-                download()
+        task = options.get("task")
+        if task == "download_only":
+            import importlib # noqa
+            module, func = get_cyklisty_sobe_layer.get_cs_features_layer_func.rsplit(".", 1)
+            module = importlib.import_module(module)
+            getattr(module, func)(
+                cache_key=get_cyklisty_sobe_layer.cache_key,
+                cache_time=get_cyklisty_sobe_layer.long_cache_time,
+                save_to_file=get_cyklisty_sobe_layer.features_file_path,
+            )
         else:
-            download()
+            def download():
+                async_task(func=get_cyklisty_sobe_layer.get_cs_features_layer_func,
+                           cache_key=get_cyklisty_sobe_layer.cache_key,
+                           cache_time=get_cyklisty_sobe_layer.long_cache_time,
+                           save_to_file=get_cyklisty_sobe_layer.features_file_path,
+                           save=True)
+
+            all_job_db_result, job_db_result, time_delta = \
+                check_download_cykliste_sobe_layer_job()
+
+            if job_db_result:
+                if (time_delta.total_seconds() >= get_cyklisty_sobe_layer.short_cache_time):
+                    download()
+                elif not get_cyklisty_sobe_layer.features_file_path.is_file():
+                    download()
+            else:
+                download()
