ca02d933cd3bf1d03e254d01135160f51a0f0576
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-13 13:16:08 +0200

    docker-entrypoint.sh: fix run Django migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,6 @@ if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
 su test -c 'for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;'
 su test -c "crontab -l >> "$CRONTAB_FILE""
 su test -c "crontab "$CRONTAB_FILE""
+# Django migrate
+su test -c "cd /home/test; pipenv run python3 manage.py migrate"
 exec /usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
