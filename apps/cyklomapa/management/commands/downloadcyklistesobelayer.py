c68f7af1656c2c3c082c13ddbdde5f5f46027615
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-13 05:58:05 +0200

    cyklomapa/management/commands/downloadcyklistesobelayer.py: refactor method code

--- apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
+++ apps/cyklomapa/management/commands/downloadcyklistesobelayer.py
@@ -4,14 +4,33 @@ import pathlib
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from cyklomapa.utils import parse_cykliste_sobe_features
+from django_q.tasks import async_task
+
+from cyklomapa.utils import (
+    check_download_cykliste_sobe_layer_job, parse_cykliste_sobe_features,
+)
 from cyklomapa.views import get_cyklisty_sobe_layer
+
 
 class Command(BaseCommand):
     """Download cykliste sobe features JSON layer files"""
     help = "Download cykliste sobe features JSON layer files" # noqa
 
     def handle(self, *args, **options):
-        parse_cykliste_sobe_features(
-            save_to_file=get_cyklisty_sobe_layer.features_file_path,
-        )
+        def download():
+            async_task(func=get_cyklisty_sobe_layer.get_cs_features_layer_func,
+                       cache_key=get_cyklisty_sobe_layer.cache_key,
+                       cache_time=get_cyklisty_sobe_layer.long_cache_time,
+                       save_to_file=get_cyklisty_sobe_layer.features_file_path,
+                       save=True)
+
+        all_job_db_result, job_db_result, time_delta = \
+            check_download_cykliste_sobe_layer_job()
+
+        if job_db_result:
+            if (time_delta.total_seconds() >= get_cyklisty_sobe_layer.short_cache_time):
+                download()
+            elif not get_cyklisty_sobe_layer.features_file_path.is_file():
+                download()
+        else:
+            download()
