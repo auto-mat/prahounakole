0bf2f8a52675ff37fea0a3036d6be090c0cfefbe
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-14 08:03:50 +0200

    docker-entrypoint.sh: remove temporary crontab file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
 su test -c '
 for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME" "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT"; do echo $i >> '$CRONTAB_FILE'; done;
 crontab -l >> '$CRONTAB_FILE'
-crontab '$CRONTAB_FILE''
+crontab '$CRONTAB_FILE'
+rm '$CRONTAB_FILE''
 # Django migrate
 su test -c "cd /home/test; pipenv run python3 manage.py migrate"
 # Run supervisord
