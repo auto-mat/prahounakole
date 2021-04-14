256b8826e6e24288d1973b4126d0b84c6fee4a5e
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-14 08:02:46 +0200

    docker-entrypoint.sh: add missing DB_HOST, DB_PORT global variables into crontab

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 CRONTAB_FILE=/tmp/crontab
 if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
 su test -c '
-for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;
+for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME" "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT"; do echo $i >> '$CRONTAB_FILE'; done;
 crontab -l >> '$CRONTAB_FILE'
 crontab '$CRONTAB_FILE''
 # Django migrate
