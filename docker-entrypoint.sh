a284be717050d1b61efa7bbfe2ceb315f5b93ae3
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-04-14 08:26:43 +0200

    docker-entrypoint.sh: if global variables DB_HOST, DB_PORT doesn't exist, don't add them into crontab

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,12 @@
 CRONTAB_FILE=/tmp/crontab
 if [ -f $CRONTAB_FILE ]; then rm $CRONTAB_FILE; fi
 su test -c '
-for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME" "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT"; do echo $i >> '$CRONTAB_FILE'; done;
+if [ -z $DB_HOST ]
+then
+     for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME"; do echo $i >> '$CRONTAB_FILE'; done;
+else
+    for i in "DB_USER=$DB_USER" "DB_PASSWORD=$DB_PASSWORD" "DB_NAME=$DB_NAME" "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT"; do echo $i >> '$CRONTAB_FILE'; done;
+fi;
 crontab -l >> '$CRONTAB_FILE'
 crontab '$CRONTAB_FILE'
 rm '$CRONTAB_FILE''
